9d854b78529dd11e5a25c70eb2584fb9fb7969f2
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2023-01-26 13:55:06 +0100

    First publisher/consumer classes

---
+++ src/main/java/zerbini/Consumer.java
@@ -0,0 +1,48 @@
+package zerbini;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+        try {
+            Connection connection = factory.createConnection();
+
+            // Establish the connection (required for Consumer)
+            connection.start();
+
+            // Options of createSession: transacted (boolean) and acknowledgeMode
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create queue if it does not exist
+            Destination destination = session.createQueue("demo");
+
+            MessageConsumer consumer = session.createConsumer(destination);
+            // Consumer is in listening mode
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    TextMessage textMessage = (TextMessage) message;
+                    try {
+                        System.out.println(textMessage.getText());
+                    } catch (JMSException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/zerbini/Publisher.java
@@ -0,0 +1,48 @@
+package zerbini;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Publisher {
+
+
+    public static void main(String[] args) {
+
+        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+        try {
+            Connection connection = factory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create queue if it does not exist already
+            Destination destination = session.createQueue("demo");
+
+            String[] messages = {
+                    "First message",
+                    "Second message",
+                    "Third message",
+                    "Fourth message"
+            };
+
+            MessageProducer producer = session.createProducer(destination);
+
+            for (String message : messages) {
+                TextMessage textMessage = session.createTextMessage(message);
+                // Publish the message to the queue
+                producer.send(textMessage);
+            }
+
+            System.out.println("Messages published");
+
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
